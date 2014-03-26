7e4c4338c85b604084712e5b6327a09beb442799
aquazul <0643839020@mail.ecc.u-tokyo.ac.jp>
2014-03-26 16:02:25 +0900

    tools.py: prepare demonstration

--- tools.py
+++ tools.py
@@ -65,3 +65,56 @@ def parse_Atomselection(L):
 def parse_AtomselectionNum(L):
     return sorted(int(i) for i in parse_Atomselection(L))
 
+if __name__ == '__main__':
+    import argparse
+    each_slice_demo = lambda L, n: list(each_slice(L, n))
+    demo = {'each_slice_demo': (range(10), 3),
+            'removeall': ([1, 0, 0, 1, 0, 1, 0, 0], 0),
+            'flatten': ([1, [2, 3, [4, 5], [6]], 7, [8, 9]],),
+            'parse_Atomselection': ('1-5, 8, 9, 11-15',),
+            }
+    argcounts = {'each_slice_demo': 2,
+                 'removeall': 2,
+                 'flatten': 1,
+                 'parse_Atomselection': 1}
+    available = ['all'] + list(demo.keys())
+    parser = argparse.ArgumentParser(description='''collection of tools used in this package.''',
+                 formatter_class=argparse.RawTextHelpFormatter,
+                 epilog='''-a/--args option arguments are interpleted by eval().
+so strings must be given with quotations("" or '').
+Because command line regards spaces as break,
+list argument must be written without any space.
+(i.e. [1,2,3,4,5] is valid, while [1, 2, 3, 4, 5] is invalid.)''')
+    parser.add_argument('choice', metavar='funcname', nargs='+',
+                        choices=available,
+                        help='''Demonstrate choosen function.
+*all* shows all function in the choice.
+If -a/--args option is given, get argument(s) from command line.
+Otherwise use prepared argument(s).''')
+    parser.add_argument('-a', '--args', metavar='values', nargs='+',
+                        action='append', dest='values',
+                        help='''Use given argument(s) for demonstration.
+You have to use this option for each function.
+See epilog for notices for argument notation.''')
+    args = parser.parse_args()
+    
+    if 'all' in args.choice:
+        choices = demo.keys()
+    else:
+        choices = args.choice
+    index = 0
+    for func in choices:
+        if args.values is None or len(args.values) <= index:
+            values = demo[func]
+        else:
+            if argcounts[func] != len(args.values[index]):
+                print('argument number not match (require {}, given {})\nuse default values.'.format(
+                      argcounts[func], len(args.values[index])))
+                values = demo[func]
+            else:
+                values = [eval(s) for s in args.values[index]]
+            index += 1
+        print('Demonstrate function {}()\ninput:  '.format(func), end='')
+        print(*values, sep=' : ')
+        print('output: ', end='')
+        print(vars()[func](*values))
