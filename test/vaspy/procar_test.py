9ef3dc6c190a88637562e7ba2d4b9b38a916f334
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2015-01-01 22:30:02 +0900

    add test script & dummy PROCAR

---
+++ test/vaspy/procar_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+import tempfile
+import numpy as np
+from vaspy.procar import PROCAR
+
+
+class TestPROCAR(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
