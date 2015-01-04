ccaf43070748842e9ee9cf9ab045f98a8636f0f0
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2015-01-04 14:22:56 +0900

    Treat the line beginning with "tot " to be ignored
     Changes to be committed:
    	modified:   vaspy/procar.py

--- vaspy/procar.py
+++ vaspy/procar.py
@@ -128,6 +128,7 @@ class PROCAR(object): # Version safety
                         section = ['phase']
                 else:
                     if section == ['orbital']:
+                        if 'tot ' in line[0:3]: continue
                         tmp = [float(i) for i in line.split()[1:-1]]
                         self.__orbital.append(tmp)
                     elif section == ['phase']:
