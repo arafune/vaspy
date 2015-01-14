8dc243958577c0a7b0bcacfe42dbdab3857d5157
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2015-01-14 22:55:18 +0900

    PEP8 free

--- test/vaspy/locpot_test.py
+++ test/vaspy/locpot_test.py
@@ -25,15 +25,17 @@ class TestLOCPOT(unittest.TestCase):
         np.testing.assert_almost_equal(
             np.array([34.5,  35.5,  36.5]),
             self.testlocpot2.average_along_axis('x'))
-        
+
     def test_average_along_axisY(self):
-        np.testing.assert_equal(np.array([ 31.,  34.,  37.,  40.]),
+        np.testing.assert_equal(np.array([31.,  34.,  37.,  40.]),
                                 self.testlocpot2.average_along_axis('Y'))
 
     def test_axis_length(self):
         x, y, z = self.testlocpot2.get_axes_lengthes()
         self.assertEqual((x, y, z),
-             (6.7395854049999997, 6.7395858129882784, 36.271284721999997))
-        
+                         (6.7395854049999997,
+                          6.7395858129882784,
+                          36.271284721999997))
+
 if __name__ == '__main__':
     unittest.main()
