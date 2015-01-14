ad912d03d862dd01c2309b619932857ed5c76468
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2015-01-14 13:02:07 +0900

    Add new file for unit test of locpot.py

---
+++ test/vaspy/locpot_test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import os
+import sys
+from vaspy.locpot import LOCPOT
+
+
+class TestLOCPOT(unittest.TestCase):
+
+    def setUp(self):
+        pass
