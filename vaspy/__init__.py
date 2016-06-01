c6596b64defe282e3fed937a2b068bb76eadb058
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2016-06-01 12:08:40 +0900

    load function...

--- vaspy/__init__.py
+++ vaspy/__init__.py
@@ -5,8 +5,8 @@
 
     modules made for VASP pre\postprocess
 """
-# import os
-
+import os.path
+import re
 __all__ = ['chgcar',
            'doscar',
            'outcar',
@@ -17,3 +17,38 @@ __all__ = ['chgcar',
            ]
 
 from vaspy import *
+
+def load(filename, type=None):
+    ''' .. py:function:: load(filename, type)
+
+    load file
+
+    :param str filename: filename
+
+    :param str type: optional argument type
+
+        * This is the optional argument, because this function judges
+    the file type mainly from its name
+        * type is poscar, outcar, chgcar, procar (case insensitive)
+    '''
+    from . import poscar, outcar, chgcar, doscar, locpot, procar
+    filenamebase = os.path.basename(filename).lower()
+    if isinstance(type, str):
+        type = type.lower()
+    if re.search(r'poscar|vasp', filenamebase) or type == 'poscar':
+        output = poscar.POSCAR(filename)
+    elif re.search(r'outcar', filenamebase) or type == 'outcar':
+        output = outcar.OUTCAR(filename)
+    elif re.search(r'chgcar', filenamebase) or type == 'chgcar':
+        output = chgcar.CHGCAR(filename)
+    elif re.search(r'procar', filenamebase) or type == 'procar':
+        output = procar.PROCAR(filename)
+    elif re.search(r'locpot', filenamebase) or type == 'locpot':
+        output = locpot.LOCPOT(filename)
+    elif re.search(r'doscar', filenamebase) or type == 'doscar':
+        output = doscar.DOSCAR(filename)
+    else:
+        raise RuntimeError("The file type cannot be identified!  Set 'type'")        
+    return output
+        
+        
