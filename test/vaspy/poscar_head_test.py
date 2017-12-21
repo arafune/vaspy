7b45ba7198e64a03f60223d127507c8f1ed28d7d
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2017-12-21 16:55:46 +0900

    revise test routine to match the pythonic way

--- test/vaspy/poscar_head_test.py
+++ test/vaspy/poscar_head_test.py
@@ -18,5 +18,4 @@ class TestPOSCAR_Head(object):
     @with_setup(setup=setup)
     def test_poscar_head(self):
         eq_('testPOSCAR', self.poscar_head.system_name)
-        eq_('#1:Ag1', self.poscar_head.atom_identifer[0])
-        
+        eq_('#0:Ag1', self.poscar_head.atom_identifer[0])
