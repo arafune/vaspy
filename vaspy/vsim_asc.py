050cd22e6c9de5edb4407fc72470799a5f0d974f
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2018-08-13 21:39:58 +0900

    add new module vsim_asc

---
+++ vaspy/vsim_asc.py
@@ -0,0 +1,15 @@
+# -*- codinng: utf-8 -*-
+'''This module provides vsim_asc class
+
+This module might not be a member of vaspy. But the input file .*ascii
+is generated usually by phonopy, and the input files needed to phonopy
+calculation is provided from the VASP calculations.
+
+Purpose of this module
+This module generates
+
+1) POSCAR files for phonon displacement
+2) POVRAY scene file for animation
+
+The first is absolutely required.
+ '''
