ccb9133dbfddd7ffbef5b57f442a1e395a738304
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2018-08-14 11:38:25 +0900

    pep8 (+3 squashed commits) Squashed commits: [8a382b5] vsim.ascii loader [c859df5] test unit for vsim_asc [45e7a55] add new example monolayer_hBN_phonon.ascii that represents K point phonon motion of hBN

---
+++ test/vaspy/test_vsim_asc.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+# import tempfile
+from nose.tools import eq_, ok_
+from nose.tools import with_setup, assert_equal, raises
+from nose.tools import assert_false
+import numpy as np
+import vaspy.vsim_asc as vsim_asc
+
+
+class TestVsimASCII(object):
+    '''Class for Test of Vsim_asc
+
+'''
+    def setup(self):
+        '''VSIM'''
+        datadir = os.path.abspath(os.path.dirname(__file__)) + "/data/"
+        datafile = datadir + "monolayer_hBN_phonon.ascii"
+        self.hBN = vsim_asc.VSIM_ASC(datafile)
+
+    @with_setup(setup=setup)
+    def test_(self):
+        eq_('Phonopy generated file for v_sim 3.6', self.hBN.system_name)
+        np.testing.assert_allclose([2.484999131, 0, 0],
+                                   self.hBN.lattice_vector[0])
+        np.testing.assert_allclose([1.242498697, 2.152072878, 0],
+                                   self.hBN.lattice_vector[1])
+        np.testing.assert_allclose([0, 0, 24.85], 
+                                   self.hBN.lattice_vector[2])
+        eq_(1, self.hBN.ionnums[0])
+        eq_(1, self.hBN.ionnums[1])
+        eq_('B', self.hBN.iontypes[0])
+        eq_('N', self.hBN.iontypes[1])
+        #
+        #
+        np.testing.assert_allclose([9.354567, 17.815346, 26.162733,
+                                   32.356041, 35.832499, 39.191721],
+                                   self.hBN.freqs)
+        #
+        np.testing.assert_allclose([0.000000 + -0.188937j,
+                                    -0.188937 + 0.000000j,
+                                    -0.000000 + 0.000000],
+                                   self.hBN.d_vectors[3][1])  #  mode is '3', second atom
+        np.testing.assert_allclose([-0.112619 -0.065020j,
+                                    0.065021 - 0.112619j,
+                                    0.0 -0.0j],
+                                   self.hBN.d_vectors[5][1])
+
--- vaspy/vsim_asc.py
+++ vaspy/vsim_asc.py
@@ -13,3 +13,122 @@ This module generates
 
 The first is absolutely required.
  '''
+
+import os.path
+import re
+import bz2
+import numpy as np
+
+
+class VSIM_ASC(object):
+    def __init__(self, filename=None):
+        self.ionnums = []
+        self.iontypes = []
+        self.system_name = ""
+        #
+        self.qpts = []
+        self.freqs = []
+        #
+        if filename:
+            if os.path.splitext(filename)[1] == '.bz2':
+                try:
+                    thefile = bz2.open(filename, mode='rt')
+                except AttributeError:
+                    thefile = bz2.BZ2File(filename, mode='r')
+            else:
+                thefile = open(filename)
+            self.load_file(thefile)
+
+    def load_file(self, thefile):
+        '''vsim.ascii parser
+
+        Parameters
+        ----------
+
+        thefile: StringIO
+            "VSIM.ascii" file
+'''
+        phonon_lines = []
+        # the first line is system name
+        self.system_name = next(thefile)[1:].strip()
+        # the 2nd line represents dxx, dyx, dyy
+        dxx, dyx, dyy = [float(x) for x in next(thefile).split()]
+        # the 3rd line represents dzx, dzy, dzz
+        dzx, dzy, dzz = [float(x) for x in next(thefile).split()]
+        self.lattice_vector = np.array([[dxx, 0, 0],
+                                       [dyx, dyy, 0],
+                                       [dzx, dzy, dzz]])
+        ions = []
+        positions = []
+        for line in thefile:
+            line = line.strip()
+            if line[0] == "#" or line[0] == "!":
+                phonon_lines.append(line[1:].strip())
+            else:
+                x, y, z, ion = line.split()
+                ions.append(ion)
+                positions.append(np.array([float(x), float(y), float(z)]))
+        self.ionnums, self.iontypes = ions_to_iontypes_ionnums(ions)
+        #
+        dis_vectors = []
+        #
+        for line in phonon_lines:
+            if 'metaData' in line:
+                aline = line[15:]
+                if aline[-1] == '\\':
+                    aline = aline[:-1]
+                modedata = aline.split(';')
+                qpt = np.array([float(x) for x in modedata[0:3]])
+                freq = float(modedata[3])
+                self.qpts.append(qpt)
+                self.freqs.append(freq)
+            elif ']' in line:
+                pass
+            else:  # displacement vector
+                vectors = [float(x) for x in line[1:-1].split(';')]
+                dis_vectors.append([vectors[0] + vectors[3]*1j,
+                                   vectors[1] + vectors[4]*1j,
+                                   vectors[2] + vectors[5]*1j])
+        n_phonons = len(self.freqs)
+        self.d_vectors = np.array(dis_vectors).reshape(n_phonons,
+                                                       sum(self.ionnums),
+                                                       3)
+        self.freqs = np.array(self.freqs)
+
+
+def ions_to_iontypes_ionnums(ions):
+    '''Return ionnums and iontypes list
+
+    Returns
+    --------
+
+    ionnums
+        list of number of ions
+
+    iontypes
+        list of ionnames
+
+
+    Example
+    -----------
+
+    >>> ions_to_iontypes_ionnums(['Si', 'Si', 'Ag', 'Ag', 'Ag', \
+                                  'Ag', 'H', 'H', 'Si'])
+    ([2, 4, 2, 1], ['Si', 'Ag', 'H', 'Si'])
+'''
+    thelast = ''
+    ionnums = []
+    iontypes = []
+    while ions:
+        ion = ions.pop(0)
+        if thelast == ion:
+            ionnums[-1] = ionnums[-1] + 1
+        else:
+            ionnums.append(1)
+            iontypes.append(ion)
+        thelast = ion
+    return ionnums, iontypes
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
