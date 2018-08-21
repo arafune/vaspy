a0f741914f0f17a59fe781e4edd2995175b3c745
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2018-08-21 19:36:45 +0900

    vaspy-xdatcar.py: --poscar option

--- scripts/vaspy-xdatcar.py
+++ scripts/vaspy-xdatcar.py
@@ -17,18 +17,44 @@ parser.add_argument('--merge',action='store_true', default=False,
 parser.add_argument('--split', metavar='basenamePOSCAR',
                    help='''Split into several POSCAR files
 The basename of POSCAR is provided by the argument''')
-parser.add_argument("xdatcarfiles", nargs="+", metavar="XDATCAR files",
+parser.add_argument('--poscar', action='store_true', default=False,
+                    help='export into XDATCAR file from POSCAR files')
+parser.add_argument("files", nargs="+",
+                    metavar="XDATCAR or POSCAR files",
                     help="XDATCAR file(s)")
 #                    
 args = parser.parse_args()
 #
+# args.merge, args.split, args.poscar はどれか一個。
+#
+assert (([args.merge, args.poscar].count(True)==1 and args.split is None) or
+        ([args.merge, args.poscar].count(True)==0 and args.split is not None)), "Option error"
+#
 if args.merge:
     xdatcars = []
-    for xdatcar_file in args.xdatcarfiles:
+    for xdatcar_file in args.files:
         xdatcars.append(XDATCAR(xdatcar_file))
     output_xdatcar = copy.deepcopy(xdatcars[0])
     configurations = []
     for xdatcar in xdatcars:
         configurations += xdatcar.configurations
     output_xdatcar.configurations = configurations
+    print(output_xdatcar)
+if args.split:
+    pass
+if args.poscar:
+    poscars = []
+    for poscar_file in args.files:
+        poscars.append(POSCAR(poscar_file))
+    output_xdatcar = XDATCAR()
+    output_xdatcar.scaling_factor = poscars[0].scaling_factor
+    output_xdatcar.system_name = poscars[0].system_name
+    output_xdatcar.cell_vecs = poscars[0].cell_vecs
+    output_xdatcar.iontypes = poscars[0].iontypes
+    output_xdatcar.ionnums = poscars[0].ionnums
+    positions = []
+    for poscar in poscars:
+        poscar.to_direct
+        positions.append( poscar.positions)
+    output_xdatcar.configurations = positions
     print(output_xdatcar)
