dcfff80a92d5d944886c518076e62d60cf10176f
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2020-05-26 18:47:25 +0900

    use black for beautify

--- scripts/vaspy-procar.py
+++ scripts/vaspy-procar.py
@@ -25,43 +25,53 @@ logger.addHandler(handler)
 logger.propagate = True
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('--outcar',
-                    metavar='outcar_file',
-                    help='''Use "OUTCAR" for the Fermi level correction.
+parser.add_argument(
+    "--outcar",
+    metavar="outcar_file",
+    help="""Use "OUTCAR" for the Fermi level correction.
 outcar_file must be specified.
 NOTE:  E-fermi of OUTCAR generated in
-Band-calculation may NOT be reliable.''')
-parser.add_argument('--fermi',
-                    metavar='value',
-                    type=float,
-                    help='''Fermi level correction
+Band-calculation may NOT be reliable.""",
+)
+parser.add_argument(
+    "--fermi",
+    metavar="value",
+    type=float,
+    help="""Fermi level correction
 Energy shifts by this value
-if --outcar is set, this option is ignored''')
-parser.add_argument('--site',
-                    metavar='atom_indices',
-                    dest='atomindex',
-                    action='append',
-                    type=tools.atom_selection_to_list,
-                    help='''atom index specifed with range.
+if --outcar is set, this option is ignored""",
+)
+parser.add_argument(
+    "--site",
+    metavar="atom_indices",
+    dest="atomindex",
+    action="append",
+    type=tools.atom_selection_to_list,
+    help="""atom index specifed with range.
 Use "-" or ","
- (ex.) --site 1,2,7-9''')
-parser.add_argument('--as',
-                    metavar='name',
-                    nargs='+',
-                    dest='atomsetname',
-                    action='append',
-                    help='''the name of the sites identified
+ (ex.) --site 1,2,7-9""",
+)
+parser.add_argument(
+    "--as",
+    metavar="name",
+    nargs="+",
+    dest="atomsetname",
+    action="append",
+    help="""the name of the sites identified
 by --site option
-the name is used in the title of the column''')
-parser.add_argument('--orbital',
-                    metavar='orbitals',
-                    action='append',
-                    type=ft.partial(re.split, r'[,:]'),
-                    help='''orbital name deliminated by ":" or ",".
+the name is used in the title of the column""",
+)
+parser.add_argument(
+    "--orbital",
+    metavar="orbitals",
+    action="append",
+    type=ft.partial(re.split, r"[,:]"),
+    help="""orbital name deliminated by ":" or ",".
 orbital names are:
 s, p, pxpy, pz, d, dxy, dyz, dz2, dxz, dx2
- (ex.) --orbital s:pxpy:d''')
-parser.add_argument('procar', metavar='PROCAR_file', help='''PROCAR file''')
+    (ex.) --orbital s:pxpy:d""",
+)
+parser.add_argument("procar", metavar="PROCAR_file", help="""PROCAR file""")
 
 args = parser.parse_args()
 logger.debug("Debugging...")
@@ -84,8 +94,9 @@ if recvec:
     logger.debug("procar.kvecs in (AA): {}".format(procar.kvecs))
 
 #
-assert len(args.atomindex) == len(args.orbital) == len(args.atomsetname), \
-    "--atom, --as and --orbital are mismatched."
+assert (
+    len(args.atomindex) == len(args.orbital) == len(args.atomsetname)
+), "--atom, --as and --orbital are mismatched."
 #
 sitenames = tuple(chain.from_iterable(args.atomsetname))
 flat_orbitals = tuple(chain.from_iterable(args.orbital))
@@ -94,35 +105,37 @@ flat_orbitals = tuple(chain.from_iterable(args.orbital))
 # internaly begins with "0".  (This is because VASP is fortran program !)
 siteindex = [[i - 1 for i in internal] for internal in args.atomindex]
 #
-logger.debug('siteindex: {}'.format(siteindex))
-logger.debug('sitenames: {}'.format(sitenames))
-logger.debug('args.orbitals: {}'.format(args.orbital))
-logger.debug('orbitals flat: {}'.format(flat_orbitals))
+logger.debug("siteindex: {}".format(siteindex))
+logger.debug("sitenames: {}".format(sitenames))
+logger.debug("args.orbitals: {}".format(args.orbital))
+logger.debug("orbitals flat: {}".format(flat_orbitals))
 #
 for sites, name in zip(siteindex, sitenames):
     procar.append_sumsite(tuple(sites), name)
 for orb in tuple(set(flat_orbitals)):
     procar.append_sumorbital(procar.orbital_index(orb), orb)
 #
-logger.debug("label['site'] is {}".format(procar.label['site']))
-logger.debug("label['orbital'] is {}".format(procar.label['orbital']))
+logger.debug("label['site'] is {}".format(procar.label["site"]))
+logger.debug("label['orbital'] is {}".format(procar.label["orbital"]))
 #
 site_indexes = []
 orbtal_indexes_sets = []
 for site in sitenames:
-    site_indexes.append(procar.label['site'].index(site))
+    site_indexes.append(procar.label["site"].index(site))
 for orbitals in args.orbital:
     tmp = []
     for orbital_in_site in orbitals:
-        tmp.append(procar.label['orbital'].index(orbital_in_site))
+        tmp.append(procar.label["orbital"].index(orbital_in_site))
     tmp = tuple(tmp)
     orbtal_indexes_sets.append(tmp)
 site_indexes = tuple(site_indexes)
 orbital_indexes_sets = tuple(orbtal_indexes_sets)
 #
-logger.debug('site_indexes: {}'.format(site_indexes))
-logger.debug('orbital_indexes_sets: {}'.format(orbital_indexes_sets))
+logger.debug("site_indexes: {}".format(site_indexes))
+logger.debug("orbital_indexes_sets: {}".format(orbital_indexes_sets))
 #
 print(
-    procar.text_sheet(site_indexes=site_indexes,
-                      orbital_indexes_sets=orbital_indexes_sets))
+    procar.text_sheet(
+        site_indexes=site_indexes, orbital_indexes_sets=orbital_indexes_sets
+    )
+)
